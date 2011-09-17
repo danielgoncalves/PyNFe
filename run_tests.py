5ce958ff2cf116e2b4314e65457862184f13bd47
Daniel Gonçalves <daniel@base4.com.br>
2011-09-16 23:20:43 -0300

    Licença nos cabeçalhos dos arquivos py e PEP8 em run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,29 +1,62 @@
-import sys, doctest, os, glob
+# -*- coding: utf-8 -*-
+#
+# PyNFe/run_tests.py
+#
+# Projeto PyNFe
+# Copyright (C) 2010 Marinho Brandão et al
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+
+import doctest
+import glob
+import os
+import sys
+
 from getopt import gnu_getopt as getopt
 
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, CUR_DIR)
 
 if __name__ == '__main__':
+
     run_level = None
-    optlist, args = getopt(sys.argv[1:], "l:", ['--level='])
     
+    optlist, args = getopt(sys.argv[1:], "l:", ['--level='])
     for opt, arg in optlist:
         if opt in ("-l", "--list"):
             run_level = arg.zfill(2)
     
-    # Test each package
+    # determina a máscara dos nomes dos arquivos que serão
+    # submetidos aos testes
     if run_level is None:
+        # todos os arquivos de texto da pasta ``tests`` serão testados
         search_path = '%s/*.txt' % os.path.join(CUR_DIR, 'tests')
-    else: search_path = '%s/%s-*.txt' % \
-        (os.path.join(CUR_DIR, 'tests'), run_level)
+    else: 
+        # apenas os arquivos que iniciam com o valor de ``run_level``
+        # serão submetidos a testes (eg. ``04-servidor-soap.txt``)
+        search_path = '%s/%s-*.txt' % (
+                os.path.join(CUR_DIR, 'tests'), run_level)
     
-    test_files = glob.glob(search_path)
-    test_files = map(lambda i: i[len(CUR_DIR)+1:], test_files)
+    # obtém uma lista de todos os nomes de arquivos (sem o caminho)
+    # que serão testados via ``doctest``
+    test_files = [os.path.split(f)[-1] for f in glob.glob(search_path)]
 
-    # Run the tests
+    # executa os testes
     for fname in test_files:
-        print 'Running "%s"...'%(os.path.splitext(os.path.split(fname)[-1])[0])
+        print 'Running "%s"...' % os.path.splitext(fname)[0]
         doctest.testfile(fname)
 
     print 'Finished!'
